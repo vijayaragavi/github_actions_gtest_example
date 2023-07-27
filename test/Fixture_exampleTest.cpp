471919f18c75fb715ec9f64dcf9324e037ac1de7
vijayaragavi <vijiguna01@gmail.com>
2023-07-27 11:43:41 +0530

    Update Fixture_exampleTest.cpp

--- test/Fixture_exampleTest.cpp
+++ test/Fixture_exampleTest.cpp
@@ -1,80 +1,64 @@
 
 #include "gtest/gtest.h"
-
 class C{};
-
 class B
 {
-
    C* ptr;
-
    public:
-
    //universal object creation
-
    B(C* addressofObject): ptr(addressofObject){
-
    }
-
 };
 
 class A
 {
-
    B* ptr;
-
    public:
-
    //universal object creation
-
    A(B* addressofObject): ptr(addressofObject){
-
    }
-
    bool operation(){
-
      return true;
-
    }
-
-
 };
-
+class TestFixtureTestSuite:public testing::Test{
+//anything we write in this class should be under protected
+protected :
+A* aPtr;
+B *bPtr;
+C* cPtr;
+TestFixtureTestSuite(){
+   //initialization
+}
+~TestFixtureTestSuite(){
+   //Releasing the Resources
+}
+void Setup(){
+   //Arrange
+   this->aptr=new C();
+   this->aptr=new B(cPtr);
+   this->aptr=new A(bPtr);
+}
+void TearDown(){
+   //delete resources
+   delete cPtr;
+   delete bPtr;
+   delete aPtr;
+}
+};
  
 
-TEST(TestFixtureObject, operationTest)
+TEST(TestFixtureTestSuite, operationTrueTest)
+{
+   //Act and assert
+   ASSERT_TRUE(aPtr->operation());
+
+}*/
+
+
+/*TEST(TestFixtureTestSuite, operationFalseTest)
 
 {
-
-    //Arrange
-
-   C cObj;
-
-   B bObj(&cObj);
-
-   A aObj(&bObj);
-
    //Act and assert
-
-   ASSERT_TRUE(aObj.operation());
-
-}
-
-
-TEST(TestFixtureObject, operationFalseTest)
-
-{
-
-    //Arrange
-
-   C cObj;
-
-   B bObj(&cObj);
-
-   A aObj(&bObj);
-
-   //Act and assert
-
-   ASSERT_FALSE(aObj.operation());
-
-}
+   ASSERT_FALSE(aPtr->operation());
+}*/
