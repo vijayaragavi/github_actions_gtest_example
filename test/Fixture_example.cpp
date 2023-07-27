b6fd25d227423d155e8e8a07cfe45336201bd194
vijayaragavi <vijiguna01@gmail.com>
2023-07-27 11:10:43 +0530

    Create Fixture_example.cpp
    Test fixture

---
+++ test/Fixture_example.cpp
@@ -0,0 +1,37 @@
+#inclde "gtest/gtest.h"
+class C();
+class B(
+C* ptr
+public:
+B(C* addressofObject: ptr(addressofObject)){
+}
+);
+class A( 
+B* ptr
+public:
+A(B* addressofObject: ptr(addressofObject)){
+}
+bool operator(){
+  return true;
+}
+);
+//Fixture test
+TEST("TestFixtureTest","OperationTest")
+{
+  //Arrange
+  C cObj;
+  B bObj(&cObj);
+  A obj(&bObj);
+  //Act and Assert
+  ASSERT_TRUE(obj.operation());  
+}
+
+TEST("TestFixtureTest","OperationFalseTest")
+{
+  //Arrange
+  C cObj;
+  B bObj(&cObj);
+  A obj(&bObj);
+  //Act and Assert
+  ASSERT_FALSE(obj.operation());  
+}
