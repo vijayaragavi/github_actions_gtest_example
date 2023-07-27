075fec0f7fd98ed521f76abe82bea87915a70a7d
vijayaragavi <vijiguna01@gmail.com>
2023-07-27 17:13:01 +0530

    Update gmocktest.cpp

--- test/gmocktest.cpp
+++ test/gmocktest.cpp
@@ -1,5 +1,44 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
+#include "gtest/gtest.h"
+using testing::Types;
+
+class ITempSensor{
+    public:
+    virtual ~ITempSensor(){};
+    virtual int getOutSideTemp()=0;
+};
+
+class ModelATempSensor:public ITempSensor{
+    public:
+    int getOutSideTemp(){ return  23;}
+};
+class ModelBTempSensor:public ITempSensor{
+    public:
+    int getOutSideTemp(){ return  23;}
+};
+class ModelCTempSensor:public ITempSensor{
+    public:
+    int getOutSideTemp(){ return  23;}
+};
+class AutoTempRegulator{
+    ITempSensor* tempSensorPtr;
+    public:
+    AutoTempRegulator(ITempSensor* address):tempSensorPtr{address} {}
+    void  regulateTemp(){
+         this->tempSensorPtr->getOutSideTemp();
+    }  
+};
+class MockTempSensor: public ITempSensor(){
+    public:
+    MOCK_METHOD(int,getOutsideTemp,());
+}
+TEST(AutoTempRegulatorTestSuite,RegulateTempInteractionTesting)
+{
+   MockTempSensor stubObj;
+   AutoTempRegulator codeUnderTest(&stubObj);
+   codeUnderTest.regulateTemp();//interaction
+}
 
 int main()
 {
