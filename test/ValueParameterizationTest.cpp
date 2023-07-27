3450fc25c2ca635f6303943977e3f23a92d4c81d
vijayaragavi <vijiguna01@gmail.com>
2023-07-27 12:32:03 +0530

    Update ValueParameterizationTest.cpp

--- test/ValueParameterizationTest.cpp
+++ test/ValueParameterizationTest.cpp
@@ -4,24 +4,21 @@ int divFun(int numerator,int denominator){
 
     return numerator/denominator;
 }
-TEST(DivFunTestSuite,HandleValidInputs){
-    int numerator=10;
-    int denomiator=5;
-    int expectedValue=2;
+class DivFunTestSuite:public testing :: TestWithParam<std::tuple<int,int,int>>{
+protected:
+DivFunTestSuite();
+~DivFunTestSuite();
+}
+TEST_P(DivFunTestSuite,HandleValidInputs){
+    int numerator=std::get<0>GetParam());
+    int denomiator=std::get<1>GetParam());
+    int expectedValue=std::get<2>GetParam());
     int actualValue=divFun(numerator,denomiator);
     ASSERT_EQ(actualValue,expectedValue);
 }
-TEST(DivFunTestSuite,HandleDivideByZero){
-    int numerator=10;
-    int denomiator=0;
-    int expectedValue=0;
-    int actualValue=divFun(numerator,denomiator);
-    ASSERT_EQ(actualValue,expectedValue);
-}
-TEST(DivFunTestSuite,HandleNegetiveDenominator){
-    int numerator=10;
-    int denomiator=-5;
-    int expectedValue=0;
-    int actualValue=divFun(numerator,denomiator);
-    ASSERT_EQ(actualValue,expectedValue);
-}
+INSTANTIATE_TEST_SUITE_P(DivFunTestSuiteParameterExample,DivFunTestSuite,
+::testing::Values(
+    std::make_tuple(10,5,2),
+    std::make_tuple(10,0,0),
+    std::make_tuple(10,-5,0),
+));
