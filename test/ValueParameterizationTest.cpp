e7a01f3ba05b8eabcb7bc14a6221410dc14a9c6c
vijayaragavi <vijiguna01@gmail.com>
2023-07-27 12:13:42 +0530

    Update ValueParameterizationTest.cpp

--- test/ValueParameterizationTest.cpp
+++ test/ValueParameterizationTest.cpp
@@ -1,33 +1,27 @@
 #include "gtest/gtest.h"
-int divFun(int numerator, int denominator){
-  if(denominator == 0 || denominator < 0)
-  {
-    return 0;
-  }
-  return numerator/denominator;
-}
+int divFun(int numerator,int denominator){
+    if(denominator==0 || denominator < 0){return 0;}
 
-TEST(DivFunTestSuite,HandleZeroInputs)
-{
-  int numerator = 10;
-  int denominator = 0;
-  int expectedValue =0;
-  int actualValue = divFun(numerator,denominator);
-  ASSERT_EQ(actualValue,expectedValue);
+    return numerator/denominator;
 }
-TEST(DivFunTestSuite,HandleValidInputs)
-{
-  int numerator = 10;
-  int denominator = 5;
-  int expectedValue =2;
-  int actualValue = divFun(numerator,denominator);
-  ASSERT_EQ(actualValue,expectedValue);
+TEST(DivFunTestSuite,HandleValidInputs){
+    int numerator=10;
+    int denomiator=5;
+    int expectedValue=2;
+    int actualValue=divFun(numerator,denomiator);
+    ASSERT_EQ(actualValue,expectedValue);
 }
-TEST(DivFunTestSuite,HandleNegativeInputs)
-{
-  int numerator = 10;
-  int denominator = -2;
-  int expectedValue =0;
-  int actualValue = divFun(numerator,denominator);
-  ASSERT_EQ(actualValue,expectedValue);
+TEST(DivFunTestSuite,HandleDivideByZero){
+    int numerator=10;
+    int denomiator=0;
+    int expectedValue=0;
+    int actualValue=divFun(numerator,denomiator);
+    ASSERT_EQ(actualValue,expectedValue);
+}
+TEST(DivFunTestSuite,HandleNegetiveDenominator){
+    int numerator=10;
+    int denomiator=-5;
+    int expectedValue=0;
+    int actualValue=divFun(numerator,denomiator);
+    ASSERT_EQ(actualValue,expectedValue);
 }
